1d624c93a47adc63b7cd30bbc3730738ff9c2578
depthso <86912923+depthso@users.noreply.github.com>
2022-08-15 19:04:51 +0100

    Fixed a bug where the server wouldn't run and added a option for downloading plugins that will help the performance

--- Install.sh
+++ Install.sh
@@ -13,13 +13,14 @@ RED='\033[0;31m'
 NC='\033[0m'
 Green='\033[0;32m'
 
-if [ -f "${FileName}" ]; then
+if [ -f "papermc.jar" ]; then
 	printf "${NC}+ Server is using ${Ram} of ram.\n\n${Green}---- SERVER IP ----\n"
 	ip route
-	printf "---- ---- ---- ---- ${NC}\nStarting Server"
-	./jdk-17.0.1+12/bin/java "-Xms${Ram}" "-Xmx${Ram}" -jar "${FileName}" --nogui
+	printf "\n"
+	printf "+ Starting Server${NC}"
+	./jdk-17.0.1+12/bin/java "-Xms${Ram}" "-Xmx${Ram}" -Dfml.queryResult=confirm -XX:+UseG1GC -jar "papermc.jar" --nogui 
 else
-	printf "${Green}+ Starting Install.${NC}\n"
+	printf "${Green}+ Starting Install...${NC}\n"
 	echo '                          [0%]\r'
 	printf "${Green}+ Running sudo apt update...${NC}\n" 
 	sudo apt update || error "The command 'sudo apt update' failed. Check your apt package-management system."
@@ -36,7 +37,22 @@ else
 	tar xzfv OpenJDK17U-jdk_arm_linux_hotspot_17.0.1_12.tar.gz
 	echo '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
 	sudo rm OpenJDK17U-jdk_arm_linux_hotspot_17.0.1_12.tar.gz
-	printf "${Green}Starting PaperMC...${NC}\n"
-	./jdk-17.0.1+12/bin/java -Xms1G -Xmx1G -jar papermc.jar --nogui
+	printf "${Green}Starting PaperMC...\n\n-------------------\n"
+	echo -n '-- Do you want to install recommended plugins to increase performance? (y/n):'
+	read doinstall
+	printf "${NC}"
+	if [ $doinstall = 'y' ]
+	then
+		mkdir plugins
+		printf "${Green}+ Starting Install of plugins...${NC}\n"
+		sudo wget -P ./plugins "https://ci.codemc.io/job/Nathat23/job/StackMob-5/lastSuccessfulBuild/artifact/target/StackMob-5.8.3.jar"
+		sudo wget -P ./plugins "https://ci.lucko.me/job/spark/332/artifact/spark-bukkit/build/libs/spark-1.9.33-bukkit.jar"
+		sudo wget -P ./plugins "https://mediafiles.forgecdn.net/files/2734/790/SAML+1.3.5.jar"
+		sudo wget -P ./plugins "https://mediafiles.forgecdn.net/files/3198/633/Clearlag.jar"
+		sudo wget -P ./plugins "https://mediafiles.forgecdn.net/files/2744/510/LaggRemover-2.0.6.jar"
+		printf "${Green}+ Installed plugins${NC}\n"
+	fi
+	./jdk-17.0.1+12/bin/java -Xms1G -Xmx1G -jar "papermc.jar" --nogui
+	printf "${Green}++ Installed server!${NC}\n"
 	printf "\n- Please agree to the eula (In eula.txt) and run this script again to run the server.\n"
 fi
