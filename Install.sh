28d7fbad714a119a560050be85a425ad813c3611
depthso <86912923+depthso@users.noreply.github.com>
2022-08-15 15:00:44 +0100

    Better install process

--- Install.sh
+++ Install.sh
@@ -3,10 +3,9 @@
 #--- Server Config ---
 
 Ram='2G'  # 2048MB // The amount of ram the server will be using.
-FileName='paper-1.19.2-125.jar' # Change this if you have renamed the server jar file.
 
 # Server ip will look something like this: 192.168.1.60 NOT 192.168.1.0/24
-#										   ------------     --------------
+#                                          ------------     --------------
 
 #--- --- --- ---
 
@@ -28,7 +27,7 @@ else
 	sudo apt-get install screen -y
 	echo '>>>>>>>                   [40%]\r'
 	printf "${Green}+ Downloading Papermc 1.19.2...${NC}\n"
-	wget -P . https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/125/downloads/paper-1.19.2-125.jar || printf "Could not download PaperMC. Check your internet."
+	wget -P . -O papermc.jar https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/130/downloads/paper-1.19.2-130.jar || printf "Could not download PaperMC. Check your internet."
 	echo '>>>>>>>>>>>>>>            [60%]\r'
 	printf "${Green}+ Downloading Java 17 32bit...${NC}\n"
 	wget -P . https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_arm_linux_hotspot_17.0.1_12.tar.gz
@@ -38,6 +37,6 @@ else
 	echo '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
 	sudo rm OpenJDK17U-jdk_arm_linux_hotspot_17.0.1_12.tar.gz
 	printf "${Green}Starting PaperMC...${NC}\n"
-	./jdk-17.0.1+12/bin/java -Xms1G -Xmx1G -jar paper-1.19.2-125.jar --nogui
-	printf "\n- Please agree to the eula (In eula.txt) and run this script again to run the server."
+	./jdk-17.0.1+12/bin/java -Xms1G -Xmx1G -jar papermc.jar --nogui
+	printf "\n- Please agree to the eula (In eula.txt) and run this script again to run the server.\n"
 fi
